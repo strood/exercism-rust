c4c3a750aaa1e8f577e047f4e32b3851b9eeb349
strood <stroodz19@gmail.com>
2023-11-20 21:06:08 -0700

    isbn-verifier

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,34 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    todo!("Is {isbn:?} a valid ISBN number?");
+    // Clean input into a digit vector
+    let mut digits = Vec::new();
+    for (i, c) in isbn.char_indices() {
+        if c.is_digit(10) {
+            digits.push(c);
+        } else if c == 'X' && i == isbn.len() - 1 {
+            digits.push(c);
+        } else if c == '-' {
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    // Check length
+    if digits.len() != 10 {
+        return false;
+    }
+
+    // Check checksum
+    let mut sum = 0;
+    for (i, c) in digits.iter().enumerate() {
+        let value = if *c == 'X' {
+            10
+        } else {
+            c.to_digit(10).unwrap()
+        };
+        sum += value * (10 - i as u32);
+    }
+
+    sum % 11 == 0
 }

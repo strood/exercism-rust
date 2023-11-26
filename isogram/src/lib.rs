5b5e61f6a7dffb38f4308ad3b0367929a14d34c7
strood <stroodz19@gmail.com>
2023-11-26 15:30:26 -0700

    clippy changes

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -5,16 +5,11 @@ const A_LCASE: u8 = b'a';
 pub fn check(candidate: &str) -> bool {
     candidate
         .bytes() // iterate through u8 bytes of string
-        .filter_map(|c| {
-            c.is_ascii_alphabetic() // Check if byte is alphabetic
-                .then(|| 1u32 << (c.to_ascii_lowercase() - A_LCASE)) // If so, shift 1 left by (c - A_LCASE) and return Some(shifted value)
-                                                                     //// Ex: z = 00000010000000000000000000000000 <= 32 bytes w/ the z bit set
-                                                                     //    // a = 00000000000000000000000000000001 <= 1 byte w/ the a bit set
-                                                                     //    //           zyxwvutsrqponmlkjihgfedcba
-        })
+        .filter(|&c| c.is_ascii_alphabetic())
+        .map(|c| 1u32 << (c.to_ascii_lowercase() - A_LCASE))
         .try_fold(0u32, |ltr_flags, ltr| {
-            (ltr_flags & ltr == 0).then(|| ltr_flags | ltr) // Bitwise AND to check if ltr is already in ltr_flags
-                                                            // If not, return Some(ltr_flags | ltr) to set the bit(bitwise OR)
+            (ltr_flags & ltr == 0).then_some(ltr_flags | ltr) // Bitwise AND to check if ltr is already in ltr_flags
+                                                              // If not, return Some(ltr_flags | ltr) to set the bit(bitwise OR)
         })
         .is_some()
 }

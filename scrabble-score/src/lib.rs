88c4b94dd9823b3fdbd902b712bc90d6d51d01d6
strood <stroodz19@gmail.com>
2024-06-09 12:09:18 -0600

    scrabble-score

---
+++ scrabble-score/src/lib.rs
@@ -0,0 +1,16 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    word.to_lowercase()
+        .chars()
+        .map(|c| match c {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+            'd' | 'g' => 2,
+            'b' | 'c' | 'm' | 'p' => 3,
+            'f' | 'h' | 'v' | 'w' | 'y' => 4,
+            'k' => 5,
+            'j' | 'x' => 8,
+            'q' | 'z' => 10,
+            _ => 0,
+        })
+        .sum()
+}

75590a430c2cb4b6bb048d76ee72b68bcbb40cea
strood <stroodz19@gmail.com>
2024-10-24 20:34:10 -0600

    custom set implement

--- custom-set/src/lib.rs
+++ custom-set/src/lib.rs
@@ -1,46 +1,83 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct CustomSet<T> {
-    // We fake using T here, so the compiler does not complain that
-    // "parameter `T` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<T>,
-    // Our items
+    items: Vec<T>
 }
 
-impl<T> CustomSet<T> {
-    pub fn new(_input: &[T]) -> Self {}
+impl<T: Clone + PartialEq + Ord> CustomSet<T> {
+    pub fn new(input: &[T]) -> Self {
+        let mut set = CustomSet {
+            items: Vec::new()
+        };
+        for item in input {
+            set.add(item.clone());
+        }
+        set.sort();
+        set
+    }
 
-    pub fn contains(&self, _element: &T) -> bool {
-        todo!();
+    fn sort(&mut self) {
+        self.items.sort();
+    }
+
+    pub fn contains(&self, element: &T) -> bool {
+        self.items.contains(element)
     }
 
     pub fn add(&mut self, _element: T) {
-        todo!();
+        if self.contains(&_element) { return; }
+        self.items.push(_element);
+        self.sort();
     }
 
-    pub fn is_subset(&self, _other: &Self) -> bool {
-        todo!();
+    pub fn is_subset(&self, other: &Self) -> bool {
+        self.items.iter().all(|item| other.contains(item))
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!();
+        self.items.len() == 0
     }
 
-    pub fn is_disjoint(&self, _other: &Self) -> bool {
-        todo!();
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        self.items.iter().all(|item| !other.contains(item))
     }
 
     #[must_use]
     pub fn intersection(&self, _other: &Self) -> Self {
-        todo!();
+        let mut set = CustomSet {
+            items: Vec::new()
+        };
+        for item in self.items.iter() {
+            if _other.contains(item) {
+                set.add(item.clone());
+            }
+        }
+        set
     }
 
     #[must_use]
     pub fn difference(&self, _other: &Self) -> Self {
-        todo!();
+        let mut set = CustomSet {
+            items: Vec::new()
+        };
+        for item in self.items.iter() {
+            if !_other.contains(item) {
+                set.add(item.clone());
+            }
+        }
+        set
     }
 
     #[must_use]
     pub fn union(&self, _other: &Self) -> Self {
-        todo!();
+        let mut set = CustomSet {
+            items: Vec::new()
+        };
+        for item in self.items.iter() {
+            set.add(item.clone());
+        }
+        for item in _other.items.iter() {
+            set.add(item.clone());
+        }
+        set
     }
 }

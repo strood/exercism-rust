4fd14a0ef3bae305b6195c6afb78427517e20f89
strood <stroodz19@gmail.com>
2024-11-03 20:55:41 -0700

    largest-series-product/

---
+++ largest-series-product/src/lib.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    SpanTooLong,
+    InvalidDigit(char),
+}
+
+pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    if span > string_digits.len() {
+        return Err(Error::SpanTooLong);
+    }
+    
+    let digits: Vec<u64> = string_digits.chars().map(|c| {
+        c.to_digit(10).map(|d| d as u64).ok_or(Error::InvalidDigit(c))
+    }).collect::<Result<Vec<u64>, Error>>()?;
+
+    Ok(digits.windows(span).map(|w| w.iter().product()).max().unwrap())
+}
